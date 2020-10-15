f14978edbb89d92a176708267f73bfca2a60163e
DeepRoychoudhury <deeproydrc@gmail.com>
2020-10-15 09:26:00 +0100

    Changes for Balanced Binary Tree

--- src/com/deep/binarytree/BalancedBinaryTree.java
+++ src/com/deep/binarytree/BalancedBinaryTree.java
@@ -4,28 +4,32 @@ import com.deep.binarytree.TreeMaxDepth.TreeNode;
 
 public class BalancedBinaryTree {
 
-	    int depth=0;
-	    public boolean isBalanced(TreeNode root) {
-	        int difference=0;
-	        if(root==null) return true;
-	        
-	        int left = dfs(root.left);
-	        int right = dfs(root.right);
-	            difference = left-right;
-	        if(difference > 1){
-	            return false;
-	        }
-	        return true;
-	    }
-	    
-	    public int dfs(TreeNode root){
-	       if(root==null) return 0;
-	       
-	        int left=dfs(root.left);
-	        int right=dfs(root.right);
-	        
-	        return Math.max(left,right)+1;
-	    }
+	int depth=0;
+    public boolean isBalanced(TreeNode root) {
+        int difference=0;
+        if(root==null) return true;
+        
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+            if(left>right){
+                difference = Math.abs(left-right);
+            }
+        else{
+            difference=Math.abs(right-left);
+        }
+        if(difference > 1){
+            return false;
+        }
+        return true && isBalanced(root.left) && isBalanced(root.right);
+    }
+    
+    public int dfs(TreeNode root){
+       if(root==null) return 0;
+       int left=dfs(root.left);
+        int right=dfs(root.right);
+        
+        return Math.max(left,right)+1;
+    }
 	    
 	
 	public static void main(String[] args) {
